a6dddc12ec2aec937141bffbbb66536e1f08bf53
adadesions <adadesions@gmail.com>
2020-01-30 13:19:44 +0700

    Add path

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,24 @@ router.get('/', function(req, res, next) {
 
 router.get('/dogs', function(req, res, next) {
   let data = fs.readFileSync('public/data/pomeranian.json', 'utf8')
-  
   data = JSON.parse(data)
 
   res.render('dogs', data);
 });
+
+router.get('/dogs/:breed', function(req, res, next) {
+  let dataPath = `public/data/${req.params.breed}.json`
+  if(fs.existsSync(dataPath)) {
+    let data = fs.readFileSync(dataPath)
+    data = JSON.parse(data)
+
+    res.render('dogs', data)
+  }
+  else {
+    res.render('error', {message: `${req.params.breed} is not found`})
+  }
+
+});
+
 
 module.exports = router;
